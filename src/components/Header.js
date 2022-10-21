db5343ccfe2efea68df93f8ed03a452283bf0076
mayanth <anothai.ssht@gmail.com>
2022-10-21 11:01:50 +0700

    Shopping Cart

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import HeaderCart from "./HeaderCart"
+const Header=()=>{
+    return(
+        <header className="header">
+            <h1>Ano-Shop.com</h1>
+            <HeaderCart/>
+        </header>
+    )
+}
+
+export default Header
